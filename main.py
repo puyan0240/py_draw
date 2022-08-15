de609163bf82b6264873f0565a8c7b3875275397
puyan0240 <matusima0604@zeus.eonet.ne.jp>
2022-08-15 15:57:44 +0900

    線描画の基本部分実装

--- main.py
+++ main.py
@@ -4,36 +4,98 @@ from tkinter import ttk
 
 mouse_x = mouse_y = 0
 mouse_old_x = mouse_old_y = 0
-mouse_right_click = mouse_left_click = 0
+mouse_in_canvas = mouse_left_pushed = False
 
 
+############################################################
+#マウス移動
+############################################################
 def mouse_move(e):
     global mouse_x,mouse_y
     global mouse_old_x,mouse_old_y
+    global mouse_in_canvas,mouse_left_pushed
 
+    if mouse_in_canvas == False:
+        return  #マウスがキャンバス内に居ない
+    if mouse_left_pushed == False:
+        return  #マウス左ボタンが押されていない
+
+    #座標更新
     mouse_old_x = mouse_x
     mouse_old_y = mouse_y
     mouse_x = e.x
     mouse_y = e.y
 
+    #キャンバスに線を描く
     canvas.create_line(mouse_x, mouse_y, mouse_old_x, mouse_old_y, fill="BLACK", width=1)
 
 
-def test(e):
-    print(str(e.keycode))
-    print(str(e.type))
-    print(str(e.keysym))
-    print(e)
+############################################################
+#マウス左ボタン押す
+############################################################
+def mouse_push_left(e):
+    global mouse_x,mouse_y
+    global mouse_old_x,mouse_old_y
+    global mouse_in_canvas,mouse_left_pushed
+
+    if mouse_in_canvas == False:
+        return  #マウスがキャンバス内に居ない
+
+    mouse_x = e.x
+    mouse_y = e.y
+    mouse_left_pushed = True
+
+
+############################################################
+#マウス左ボタン離す
+############################################################
+def mouse_release_left(e):
+    global mouse_x,mouse_y
+    global mouse_old_x,mouse_old_y
+    global mouse_in_canvas,mouse_left_pushed
+
+    mouse_x = mouse_y = mouse_old_x = mouse_old_y = 0
+    mouse_left_pushed = False
+
+
+############################################################
+#マウスがキャンバスに入った
+############################################################
+def mousu_enter_canvas(e):
+    global mouse_in_canvas
+    mouse_in_canvas = True
+    canvas.config(bg="blue")    #仮
+
+
+############################################################
+#マウスがキャンバスから出た
+############################################################
+def mousu_leave_canvs(e):
+    global mouse_in_canvas
+    mouse_in_canvas = False
+    canvas.config(bg="white")   #仮
+
+    #マウス監視状態をクリアする
+    mouse_release_left(e)
+
 
 
 root = tkinter.Tk()
 root.title("Draw")
 #root.geometry("600x400")
-root.bind("<Motion>", mouse_move)
-root.bind("<Button-1>", test)
-root.bind("<ButtonRelease-1>", test, "+")
 
-canvas = tkinter.Canvas(root, width=400, height=600, bg="WHITE")
-canvas.pack()
+root.bind("<Motion>", mouse_move)
+root.bind("<Button-1>", mouse_push_left)
+root.bind("<ButtonRelease-1>", mouse_release_left)
+
+
+frame_left = tkinter.Frame(root, width=250)
+frame_left.pack(side=tkinter.LEFT, fill=tkinter.Y)
+
+canvas = tkinter.Canvas(root, bg="WHITE", width=800, height=800)
+canvas.bind("<Enter>", mousu_enter_canvas)
+canvas.bind("<Leave>", mousu_leave_canvs)
+
+canvas.pack(side=tkinter.LEFT)
 
 root.mainloop()
